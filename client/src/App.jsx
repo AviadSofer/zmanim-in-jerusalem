8fdcad69d67655b1fd9955a983d5622b251988ac
AviadSofer <aviadsofer@gmail.com>
2022-01-11 01:52:53 +0200

    try

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,20 +14,17 @@ const useStyles = makeStyles(theme => ({
     fontSize: '3rem',
     fontFamily: 'Secular One, sans-serif',
     [theme.breakpoints.down('sm')]: {
-      fontSize: '1.3rem',
+      fontSize: '0.3rem',
       maxHeight: '100vh'
     },
   },
-  textValue: {
-    maxHeight: '100vh'
-  }
 }))
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <TimeByCity className={classes.textValue} city={'ירושלים'}/>
+      <TimeByCity city={'ירושלים'}/>
     </div>
   )
 }
